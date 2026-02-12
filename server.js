7296d12d89f87d23cf09e300855b7678d1962671
singharjun048-cyber <singharjun048@gmail.com>
2026-02-12 17:20:05 +0530

    Set up MongoDB connection and API routes
    Added MongoDB connection and listings route.

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+// Import Routes
+const listingsRoute = require("./routes/listings");
 
+// Use Routes
+app.use("/api/listings", listingsRoute);
+
+// Root Test Route
 app.get("/", (req, res) => {
   res.send("MyUltrasound Backend Running");
 });
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
